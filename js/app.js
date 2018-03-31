4e8fc290f14f7ba0716a53b846a3380098494c92
Sandrikova Maria <mashasandrikova@gmail.com>
2018-03-31 12:47:49 +0100

    style: JS code style

--- js/app.js
+++ js/app.js
@@ -1,65 +1,80 @@
-/*
- * Create a list that holds all of your cards
- */
-const CARD_TYPES = ['fa-diamond', 'fa-anchor', 'fa-cube', 'fa-leaf', 'fa-bicycle', 'fa-bomb', 'fa-paper-plane-o', 'fa-bolt'];
+// Base icons for cards
+const CARD_TYPES = ['fa-diamond', 'fa-anchor', 'fa-cube', 'fa-leaf',
+    'fa-bicycle', 'fa-bomb', 'fa-paper-plane-o', 'fa-bolt'];
 
-const DECK = document.querySelector('.deck');
-const RESTART_BUTTON = document.querySelector('.restart');
-const MOVES_COUNTER = document.querySelector('.moves');
-const MINUTES = document.querySelector('.minutes');
-const SECONDS = document.querySelector('.seconds');
-const MODAL_BODY = document.querySelector('.modal-body');
-const PLAY_AGAIN = document.querySelector('.play-again');
-const STARS = document.querySelectorAll('.fa-star');
+// Stars thresholds
+const TWO_STARS_MOVES = 30;
+const ONE_STAR_MOVES = 45;
 
-const TWO_STARS_MOVES = 5;
-const ONE_STAR_MOVES = 10;
+const ELEMENTS = {
+    deck: document.querySelector('.deck'),
+    restartButton: document.querySelector('.restart'),
+    movesCounter: document.querySelector('.moves'),
+    minutes: document.querySelector('.minutes'),
+    seconds: document.querySelector('.seconds'),
+    modalBody: document.querySelector('.modal-body'),
+    playAgainButton: document.querySelector('.play-again'),
+    stars: document.querySelectorAll('.fa-star')
+};
 
-function extractType(card) {
-    return card.querySelector('i').classList[1];
-}
-
-let GAME = {
+const GAME = {
     openCards: [],
     moves: 0,
     totalTime: 0,
     matchedCards: 0,
+    start() {
+        this.clean();
+        ELEMENTS.deck.innerHTML = '';
+        let cards = [...CARD_TYPES, ...CARD_TYPES];
+        shuffle(cards);
+        for(const card of cards) {
+            ELEMENTS.deck.innerHTML  += `<li class="card">
+            <i class="fa ${card}"></i>
+            </li>`;
+        }
+    },
     clean() {
         this.openCards = [];
         this.moves = 0;
         this.totalTime = 0;
         this.matchedCards = 0;
-        MINUTES.innerText = '00';
-        SECONDS.innerText = '00';
-        MOVES_COUNTER.innerText = 0;
-        for(let star of STARS) {
+        ELEMENTS.minutes.innerText = '00';
+        ELEMENTS.seconds.innerText = '00';
+        ELEMENTS.movesCounter.innerText = 0;
+        for(let star of ELEMENTS.stars) {
             star.style.color = 'black';
         }
     },
     incMoves() {
         this.moves++;
-        MOVES_COUNTER.innerText = this.moves.toString();
+        ELEMENTS.movesCounter.innerText = this.moves.toString();
         if(this.moves == TWO_STARS_MOVES) {
-            STARS[2].style.color = 'white';
+            ELEMENTS.stars[2].style.color = 'white';
         }
         if(this.moves == ONE_STAR_MOVES) {
-            STARS[1].style.color = 'white';
+            ELEMENTS.stars[1].style.color = 'white';
         }
     },
     incMatched() {
         this.matchedCards += 2;
-        //if(this.matchedCards.length == 2 * CARD_TYPES.length ) {
+        if(this.matchedCards == 2 * CARD_TYPES.length) {
             $('#winModal').modal('show');
-            MODAL_BODY.innerHTML = `<p><i class="fa fa-star modal-star"></i> <i class="fa fa-star modal-star"></i> <i class="fa fa-star modal-star"></i></p>
-            <p>Your time is ${pad(parseInt(GAME.totalTime / 60))}:${pad(GAME.totalTime % 60)} and you made ${this.moves} moves.</p>
+            ELEMENTS.modalBody.innerHTML = `
+            <p>
+                <i class="fa fa-star modal-star"></i> 
+                <i class="fa fa-star modal-star"></i> 
+                <i class="fa fa-star modal-star"></i>
+            </p>
+            <p>Your time is ${pad(parseInt(this.totalTime / 60))}:${pad(this.totalTime % 60)} 
+            and you made ${this.moves} moves.</p>
             <p>Try to improve your result!</p>`;
-        //}
-        const stars = document.querySelectorAll('.modal-star');
-        if(this.moves >= TWO_STARS_MOVES) {
-            stars[2].style.color = 'white';
-        }
-        if(this.moves >= ONE_STAR_MOVES) {
-            stars[1].style.color = 'white';
+            const stars = document.querySelectorAll('.modal-star');
+            if(this.moves >= TWO_STARS_MOVES) {
+                stars[2].style.color = 'white';
+            }
+            if(this.moves >= ONE_STAR_MOVES) {
+                stars[1].style.color = 'white';
+            }
         }
     },
     addOpen(card) {
@@ -72,7 +87,7 @@ let GAME = {
                     openCard.classList.remove('open');
                     openCard.classList.add('match');
                 }
-                this.incMatched()
+                this.incMatched();
                 this.openCards = [];
             } else {
                 setTimeout(() => {
@@ -82,14 +97,48 @@ let GAME = {
                     this.openCards = [];
                 }, 600);
             }
-            
         }
+    }
+};
+
+// Init the game
+ELEMENTS.deck.addEventListener('click', function(evt) {
+    if (evt.target.nodeName === 'LI') {
+        if(!evt.target.classList.contains('open')
+            && !evt.target.classList.contains('match')) {
+            GAME.addOpen(evt.target);
+        }
+    }
+});
+
+ELEMENTS.restartButton.addEventListener('click', () => {GAME.start();});
+ELEMENTS.playAgainButton.addEventListener('click', () => {GAME.start();});
+
+setInterval(setTime, 1000);
+GAME.start();
+
+//////////////////////////////////////////////////////////////////
+// Utils
+
+// Timer function from https://stackoverflow.com/a/5517836/5925625
+function setTime() {
+    ++GAME.totalTime;
+    ELEMENTS.seconds.innerHTML = pad(GAME.totalTime % 60);
+    ELEMENTS.minutes.innerHTML = pad(parseInt(GAME.totalTime / 60));
+}
+
+function pad(val) {
+    const valString = val.toString();
+    if (valString.length < 2) {
+        return '0' + valString;
+    } else {
+        return valString;
     }
 }
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex;
+    let currentIndex = array.length, temporaryValue, randomIndex;
 
     while (currentIndex !== 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -100,54 +149,7 @@ function shuffle(array) {
     }
 }
 
-
-function initGame(deck, cardTypes, game) {
-    game.clean();
-    deck.innerHTML = '';
-    let cards = [...cardTypes, ...cardTypes];
-    shuffle(cards);
-    for(const card of cards) {
-        deck.innerHTML  += `<li class="card">
-        <i class="fa ${card}"></i>
-        </li>`;
-    }
+function extractType(card) {
+    return card.querySelector('i').classList[1];
 }
 
-
-DECK.addEventListener('click', function(evt) {
-    if (evt.target.nodeName === 'LI') {
-        if(!evt.target.classList.contains('open') 
-            && !evt.target.classList.contains('match')) {
-            GAME.addOpen(evt.target);
-        }
-    }
-});
-
-RESTART_BUTTON.addEventListener('click', function() {
-    initGame(DECK, CARD_TYPES, GAME);
-});
-
-PLAY_AGAIN.addEventListener('click', function() {
-    initGame(DECK, CARD_TYPES, GAME);
-});
-
-// Timer function from https://stackoverflow.com/a/5517836/5925625
-function setTime() {
-    ++GAME.totalTime;
-    SECONDS.innerHTML = pad(GAME.totalTime % 60);
-    MINUTES.innerHTML = pad(parseInt(GAME.totalTime / 60));
-}
-
-function pad(val) {
-    var valString = val + "";
-    if (valString.length < 2) {
-        return "0" + valString;
-    } else {
-        return valString;
-    }
-}
-
-// Init timer
-setInterval(setTime, 1000);
-
-initGame(DECK, CARD_TYPES, GAME);
